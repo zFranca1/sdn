290328728f44d6210382f00f6afef9412fd6a4eb
zFranca1 <joaopedro.franca1@hotmail.com>
2024-06-13 19:08:23 -0300

    alteracoes

--- experimento2.py
+++ experimento2.py
@@ -6,6 +6,10 @@ log = core.getLogger()
 def handle_PacketIn(event):
     packet = event.parsed
     
+    # Porta física de entrada (onde o pacote entrou no switch)
+    in_port = event.port
+    
+    
     # Verificando se é um pacote IPv4
     if packet.type == packet.IP_TYPE:
         log.info('\nIPv4 Packet:')
@@ -14,20 +18,12 @@ def handle_PacketIn(event):
         log.info("IP destino: %s", packet.payload.dstip)
         log.info("MAC origem: %s", packet.src)
         log.info("MAC destino: %s", packet.dst)
-        
-        # Verificando o protocolo do pacote IPv4
-        if packet.payload.protocol == packet.payload.TCP_PROTOCOL:
-            log.info("Porta de origem: %s", packet.payload.payload.srcport)
-            log.info("Porta de destino: %s", packet.payload.payload.dstport)
-        elif packet.payload.protocol == packet.payload.UDP_PROTOCOL:
-            log.info("Porta de origem: %s", packet.payload.payload.srcport)
-            log.info("Porta de destino: %s", packet.payload.payload.dstport)
+        log.info("Porta do switch: %s", in_port)    
     
     # Acessando a VLAN TCI da entrada da tabela de fluxo
     vlan_tci = packet.find('vlan').vid if packet.find('vlan') is not None else 0x0000
     log.info("VLAN: %s", vlan_tci)
+    
 
 def launch():
     core.openflow.addListenerByName("PacketIn", handle_PacketIn)
-
-
